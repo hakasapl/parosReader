2bc4e7388b38d92374ee83b8d57c918070f24d1c
Westy <westy@cs.umass.edu>
2019-04-19 12:59:30 +0000

    added setup

--- src/run.sh
+++ src/run.sh
@@ -2,11 +2,11 @@
 set -x
 #nohup ./wxtlogger_t &
 cd voltage_anemometer
-nohup python ./WindSpeedLogger.py &
+python ./WindSpeedLogger.py &
 #screen -d -m ./voltage_anemometer/WindSpeedLogger.py
 cd ..
 cd dqLogger
-nohup ./dqLogger.py &
+./dqLogger.py &
 cd ..
 ps aux | grep python > pid.txt
 #screen -d -m ./dqLogger/dqLogger.py
