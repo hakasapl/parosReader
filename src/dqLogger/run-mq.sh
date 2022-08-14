2f061ff04198f1bf4cca617cf41a1232d022c6e1
hakasapl <hakansaplakog@gmail.com>
2022-08-15 00:54:47 +0100

    disabled persistent run files

--- src/dqLogger/run-mq.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 dqLogger.py -d /opt/DQLOG -n 2 -i pc14.cloudlab.umass.edu -u paros -p testpass521
--- src/voltage_anemometer/run-mq.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 WindSpeedLogger.py -d /opt/ANIMLOG -i pc14.cloudlab.umass.edu -u paros -p testpass521
